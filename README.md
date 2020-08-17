abb2b7184a5f9f45e2bb5f32a215770bf291852b
Ivan Vladimirovich <sferadevelop@gmail.com>
2020-08-17 21:39:11 +0300

    0.0.3 restrict frequency of request

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # www
 
 ## Description
-Wrapper for native nodejs request
-support only get text in utf-8
+Wrapper for native nodejs request for request 
+to [JSON API "Мой склад"](https://dev.moysklad.ru/doc/api/remap/1.2/#mojsklad-json-api)
+support only get text in utf-8  
+The aim of this lib is restrict frequency of request 
+according to [documentation](https://dev.moysklad.ru/doc/api/remap/1.2/#mojsklad-json-api-obschie-swedeniq-obrabotka-oshibok)
 
+## Feature
+* restrict frequency of request 
+
+## version history
+
+### 0.0.3
+ * restrict frequency of request by check header [X-RateLimit-Remaining](https://dev.moysklad.ru/doc/api/remap/1.2/#mojsklad-json-api-obschie-swedeniq-obrabotka-oshibok)
 
 ## debug
-key for [debug](https://github.com/visionmedia/debug) www:info, www:debug
+key for [debug](https://github.com/visionmedia/debug) www:info, www:debug, www:err
+
+
---
+++ src/validateStatusCode.js
@@ -0,0 +1,47 @@
+const d = require('debug');
+const info = d('www:info');
+const debug = d('www:debug');
+
+/**
+ *
+ * @param {http.IncomingMessage} res
+ * @param {Object} options for http.request()
+ * @param {function} resolve callback of Promise
+ * @param {function} reject callback of Promise
+ * @param {function} request for call recursive itself if redirect
+ * @param {string} url
+ // * @alias module:www~validateStatusCode
+ * @private
+ * @return {boolean}
+ */
+function validateStatusCode(res, options, resolve, reject, request, url) {
+    let requestIsDone = true;
+    const { headers, statusCode } = res;
+    const location = headers["location"];
+    switch (statusCode) {
+        case 200:
+        case 202:
+        case 201:
+            break;
+        case 301:
+        case 302:
+        case 303:
+        case 307:
+        case 308:
+            requestIsDone = false;
+            info(
+                `Redirect url: ${url} statusCode: ${statusCode}`
+            );
+            request(location, options).then(resolve, reject);
+            break;
+        case 401:
+            reject(new Error(`Unauthorized ${headers["www-authenticate"]}`));
+            break;
+        default:
+            debug(`StatusCode: ${statusCode}`);
+    }
+    return requestIsDone;
+}
+
+
+module.exports = validateStatusCode;
